f78a53bc72d0478b95c483b85c067f82176cbc63
Willi Commer <willicommer@gmail.com>
2018-10-27 04:04:29 +0200

    1.0.4

--- bin/index.js
+++ bin/index.js
@@ -6,16 +6,18 @@
  * 
  */
 
-var fs          = require('fs');
-var path        = require('path');
-var _           = require('lodash');
-var opn         = require('opn');
-var config      = require('./../config.js');
+const fs          = require('fs');
+const path        = require('path');
+const Url         = require('url');
+const _           = require('lodash');
+const opn         = require('opn');
+const config      = require('./../config.js');
 var fileName, mdContent, htmlContent, output, outputPath;
 var test        = false;
+//var test        = true;
 
 
-// console.log('config: ',config);
+if (test) console.log('config: ',config);
 
 /*     process comand line      */
 
@@ -31,7 +33,7 @@ program
 program.parse(process.argv);
 
 if (test) {
-  fileName = path.normalize(__dirname + '/../README.md');
+   fileName = path.normalize(__dirname + '/../test/test.md');
 } else {
   if (typeof fileName === 'undefined') {
     console.error('no file given!');
@@ -40,7 +42,7 @@ if (test) {
 }  
 
 console.log('markdown file:', fileName);
-console.log('__dirname:', __dirname);
+if (test) console.log('__dirname:', __dirname);
 
 
 /*     set output path      */
@@ -48,36 +50,76 @@ console.log('__dirname:', __dirname);
 outputPath = config.output ? config.output : '../temp/result.html';
 outputPath = path.join(__dirname, outputPath);
 
-/*     resd md file      */
+/*     read md file      */
 
 mdContent = fs.readFileSync(fileName,'utf-8');
 
 
-/*     create html content      */
 
-var hljs = require('highlight.js');
-var md = require('markdown-it')({
-  html: true,
-  highlight: function (str, lang) {
-    if (lang && hljs.getLanguage(lang)) {
-      try {
-        return hljs.highlight(lang, str).value;
-      } catch (__) {}
-    }
+/* create html content      
+ * using showdown
+ */
 
-    return ''; // use external default escaping
-  }
+var showdown  = require('showdown');
+var showdownHighlight = require("showdown-highlight");
+
+var converter = new showdown.Converter({
+  
+  extensions: [showdownHighlight],
+  tables: true,
+  tasklists: true,
+  emoji: true,
+  parseImgDimensions: true
+  
 });
-htmlContent = md.render(mdContent); 
+
+htmlContent   = converter.makeHtml(mdContent);
+
+
+
 
 /*     create html page from template      */
 
-let filename        = path.basename(fileName,'.md');
-let title           = config.title.replace('[filename]', filename);
-let highlight_style = config.highlight_style || 'github';
-let style           = config.style || 'github';
+// title
+let fp;
+let filename = path.basename(fileName,'.md');
+let title    = config.title.replace('[filename]', filename);
 
-output = template(htmlContent, {title: title, style: style, highlight_style: highlight_style});
+// template
+let template = path.join(__dirname, '../template/template.html');
+template = fs.readFileSync(template,'utf-8');
+
+// page style
+config.page_style = config.page_style || 'github';
+if (config.page_style.charAt(0) !== '<') {    // is it a file or html
+  fp = require.resolve('../styles/' + config.page_style + '.css');
+  if (fp) 
+    config.page_style = '<style>' + fs.readFileSync(fp,'utf-8') + '</style>';
+  else
+    config.page_style = '';
+}
+
+// highlight style
+config.highlight_style = config.highlight_style || '';
+if (config.highlight_style) {
+  if (config.highlight_style.charAt(0) !== '<') {    // is it a file or html
+    fp = path.join(require.resolve('highlight.js'), '../../styles/', config.highlight_style + '.css');
+    if (fp) 
+      config.highlight_style = '<style>' + fs.readFileSync(fp,'utf-8') + '</style>';
+    else
+      config.highlight_style = '';  
+   }
+ }
+
+// output
+output = _.template(template)({
+  'title':   title,
+  'content': htmlContent,
+  'pageStyle': config.page_style,
+  'highlightStyle': config.highlight_style
+});
+
+output = changeLocalImageSrcToFilePath( output, fileName);
 
 /*     save it to temp folder      */
 
@@ -89,25 +131,44 @@ opn(outputPath);
 
 /*     finish exit process      */
 
-//----------------
 
 
-function template(content, option) {
-    if (test) console.log('option: %j', option); 
-
-    let templatePath            = path.join(__dirname, '../template/template.html');
-    let title                   = option.title;
-    let markdownCssPath         = require.resolve('../styles/' + option.style + '.css');
-    let highlightJsThemeCssPath = path.join(require.resolve('highlight.js'), '../../styles/', option.highlight_style + '.css');
-    let templateString          = fs.readFileSync(templatePath,'utf-8');
-    let githubMarkdownCss       = fs.readFileSync(markdownCssPath,'utf-8');
-    let highlightJsThemeCss     = fs.readFileSync(highlightJsThemeCssPath,'utf-8');
-    let output = _.template(templateString)({
-            title,
-            content,
-            githubMarkdownCss,
-            highlightJsThemeCss
-        });
-    return output;
-
+/**
+ * find all like <img src="local.png"..> and change src to absolute 'file://' link
+ * 
+ * @param {String} source HTML text 
+ * @param {String} filePath destination path for 'file://'
+ * @returns {String}
+ */
+function changeLocalImageSrcToFilePath (source, filePath) {
+console.log(filePath);  
+  // get filePath directory
+  filePath = path.dirname(path.resolve(fileName)) + path.sep;
+console.log(filePath);  
+  
+  let result=[], m, pos=0;
+ 
+  // parse <img src={}> 
+  let rex = /<img[^>]+src="([^">]+)"/gi;
+  while ( m = rex.exec(source) ) {
+    // m[0] = '<img xy src="http://.."'   m[1] = 'http://..'
+    if (getUrlProtocol(m[1]) === '') {
+      let pos2 = m.index + m[0].length - m[1].length - 1;
+      result.push(source.slice(pos, pos2));
+      pos = m.index + m[0].length-1;
+      let u = Url.resolve('file:///'+filePath, m[1]);
+      result.push(u);
+    }
+  }
+  result.push(source.slice(pos));
+  return result.join('');
 }
+
+function getUrlProtocol (url) {
+  try {
+   let u = new Url(url);
+   return u.protocol;
+  } catch(e) {};
+  return '';
+}
+
